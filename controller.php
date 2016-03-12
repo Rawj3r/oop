e9b04fc4c6ff34e3a8f7407d3ebdf55068d1084e
--replace-all <--replace-all>
2016-03-12 21:20:33 +0200

    updates

--- controller.php
+++ controller.php
@@ -5,7 +5,6 @@ require_once 'character/human.class.php';
 require_once 'character/wolf.class.php';
 require_once 'programming.class.php';
 require_once 'Talk.class.php';
-
 new Dragon;
 new Programming;
 
